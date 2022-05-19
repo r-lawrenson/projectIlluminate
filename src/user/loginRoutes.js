a010fd106efa0591a3c6b8064bf053b700305f21
r-lawrenson <100413564+r-lawrenson@users.noreply.github.com>
2022-05-19 11:55:01 +0100

    Delete src directory

--- src/user/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express') ;
-const { login } = require('../middleware');
-
-const loginRouter = Router();
-
-loginRouter.post('/login', login)
-
-module.exports = loginRouter;
