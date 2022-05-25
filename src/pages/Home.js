d60ac854ae46a3334a648b51e8bac15791ec418e
Michael <michaelf6988@gmail.com>
2022-05-25 13:43:50 +0100

    buttons link to sign up/login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
-import { SignUp } from "./SignUp"
+import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 export const Home = () => {
+    const navigate = useNavigate();
     return (
         <div>
             <h1>HomePage</h1>
-            <button onClick={<SignUp />}>Sign Up</button>
-            <button>Log In</button>
+            <button onClick={()=> navigate('/signup')}>Sign Up</button>
+            <button onClick={()=> navigate('/login')}>Log In</button>
             <h1>"YOU ARE MORE PRECIOUS TO THIS WORLD THAN YOU'LL EVER KNOW"</h1>
         </div>
     )
