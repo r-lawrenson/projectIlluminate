4171ca8c4e7c8d47e54a9d54751dcac1c50612ed
Michael <michaelf6988@gmail.com>
2022-05-25 12:27:11 +0100

    button and quote added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
+import { SignUp } from "./SignUp"
+
 export const Home = () => {
     return (
         <div>
             <h1>HomePage</h1>
+            <button onClick={<SignUp />}>Sign Up</button>
+            <button>Log In</button>
+            <h1>"YOU ARE MORE PRECIOUS TO THIS WORLD THAN YOU'LL EVER KNOW"</h1>
         </div>
     )
 }
