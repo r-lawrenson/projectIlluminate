f3b9fc69999ff1d9c592f94b60aa9b11043002df
Jade-louise simm <Jade-louisesimm1991@outlook.com>
2022-05-26 10:42:49 +0100

    added login and log out fns

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,6 +11,27 @@ export const fetchImages = async (setter) => {
 // fetch request for 30 images.
 
 
+// export const signUp = async (username, email, password, setter) => {
+// 	try {
+// 		const res = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+// 			method:"POST",
+// 			headers: {"Content-Type": "application/json"},
+// 			body: JSON.stringify({
+// 				username: username,
+// 				email: email,
+// 				password: password,
+// 			})
+
+// 		})
+// 		const data = await res.json()
+// 		setter(data.user)
+
+// 	} catch (error) {
+// 		console.log(error)
+// 	}
+// }
+
+
 export const signUp = async (username, email, password, setter) => {
 	try {
 		const res = await fetch(`${process.env.REACT_APP_REST_API}user`, {
@@ -24,9 +45,35 @@ export const signUp = async (username, email, password, setter) => {
 
 		})
 		const data = await res.json()
-		setter(data.user)
+		setter(data.user.username)
+		localStorage.setItem('myToken', data.token)
 
 	} catch (error) {
 		console.log(error)
 	}
+}
+
+export const login = async (setter) => {
+try {
+	const res = await fetch(`${process.env.REACT_APP_REST_API}user`, {
+		method:'GET',
+		headers: {Authorization: localStorage.getItem('myToken')}
+	})
+	const data = await res.json()
+	setter(data.user)
+} catch (error) {
+	console.log(error)
+}
+}
+
+export const logout = async (e, setUser, setAuth) => {
+	e.preventDefault()
+	const res = await fetch(`${process.env.REACT_APP_APP_URL}/users/logout`, {
+		method: 'GET',
+		headers: {'Authorization': `${localStorage.getItem('MyToken')}`}
+	})
+	await res.json()
+	setUser('')
+	localStorage.removeItem('MyToken')
+	setAuth(false)
 }
