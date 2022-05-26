5856a2b8ef754fff538f1c751c55552f9fbde619
Michael <michaelf6988@gmail.com>
2022-05-26 14:35:14 +0100

    user profile links

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,7 +1,21 @@
+import illuminate from '../images/illuminate-logos_black.png'
+import '../userprofile.css'
+import React from 'react'
+import { useNavigate } from "react-router-dom"
+
 export const UserProfile = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1>User Profile</h1>
+        <div className='userprofile'>
+            <img src={illuminate} alt='Illuminate Logo'/>
+            <div className='userlinks'>
+            <h1>Welcome Username</h1>
+            <div className='buttons'>
+            <button onClick={() => navigate('/journal')}>Journal</button>
+            <button onClick={() => navigate('/helpfullinks')}>Mental Health Search</button>
+            <button onClick={() => navigate('/tranquilzone')}>Tranquil Zone</button>
+            </div>
+          </div>
         </div>
     )
 }
